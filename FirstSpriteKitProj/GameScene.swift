8bb030e6707239edc3b270554a693380b0c35322
kickinbahk@gmail.com <kickinbahk@gmail.com>
2018-01-02 08:42:47 -0800

    add projectiles to screen and fire in direction of touch

--- FirstSpriteKitProj/GameScene.swift
+++ FirstSpriteKitProj/GameScene.swift
@@ -8,6 +8,37 @@
 
 import SpriteKit
 
+func + (left:  CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func - (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func * (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+func / (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+#if !(arch(x86_64) || arch(arm64))
+    func sqrt(a: CGFloat) -> CGFloat {
+        return CGFloat(sqrtf(Float(a)))
+    }
+#endif
+
+extension CGPoint {
+    func length() -> CGFloat {
+        return sqrt(x*x + y*y)
+    }
+
+    func normalized() -> CGPoint {
+        return self / length()
+    }
+}
 class GameScene: SKScene {
     let player = SKSpriteNode(imageNamed: "player")
     
@@ -42,4 +73,32 @@ class GameScene: SKScene {
         let actionMoveDone = SKAction.removeFromParent()
         monster.run(SKAction.sequence([actionMove, actionMoveDone]))
     }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        
+        let touchLocation = touch.location(in: self)
+        
+        let projectile = SKSpriteNode(imageNamed: "projectile")
+        projectile.position = player.position
+        
+        let offset = touchLocation - projectile.position
+        
+        if (offset.x < 0) { return }
+        
+        addChild(projectile)
+        
+        let direction = offset.normalized()
+        let shootAmount = direction * 1000
+        
+        let realDest = shootAmount + projectile.position
+        let actionMove = SKAction.move(to: realDest, duration: 2.0)
+        let actionMoveDone = SKAction.removeFromParent()
+        
+        projectile.run(SKAction.sequence([actionMove, actionMoveDone]))
+        
+    }
+    
+
+
 }
